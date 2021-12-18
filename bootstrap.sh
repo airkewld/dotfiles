5b2acd48b66631461c86643d4fae0c553a8d04de
Oscar Rodriguez <orodriguez@conquestcyber.com>
2021-12-17 23:47:37 -0500

    add pre-commit

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,7 @@ brew install ansible
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-
+pre-commit install
 
 ## Run playbook
 ansible-playbook config.yaml --diff
