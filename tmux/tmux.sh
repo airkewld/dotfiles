bb6b3097173cc8db4965b1f895d55f8f82bbd532
Oscar Rodriguez <orodriguez@conquestcyber.com>
2022-01-13 17:03:08 -0500

    rollback

--- tmux/tmux.sh
+++ tmux/tmux.sh
@@ -10,8 +10,7 @@ tmux has-session -t "DSO" 2> /dev/null || TMUX='' tmux new-session -d -c "$selec
 
 if [[ -z "$TMUX" ]];
 then
-tmux new-window -d -c "$selected"
-tmux attach-session -d -c "$selected" -t DSO
+tmux attach-session -d -t DSO
 else
 tmux new-window -c "$selected"
 fi
