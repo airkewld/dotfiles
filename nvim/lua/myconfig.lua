60694e9a31093734f6220fa809679cd0676a2caf
Oscar Rodriguez <orodriguez@conquestcyber.com>
2022-01-15 00:50:29 -0500

    cleanup

--- nvim/lua/myconfig.lua
+++ nvim/lua/myconfig.lua
@@ -41,37 +41,6 @@ require'lspconfig'.vimls.setup{}
 
 require'lspconfig'.yamlls.setup{}
 
--- set up compe
--- require'compe'.setup  {
---   enabled = true;
---   autocomplete  = true;
---   debug = false;
---   min_length  = 1;
---   preselect = 'enable';
---   throttle_time = 80;
---   source_timeout = 200;
---   incomplete_delay = 400;
---   allow_prefix_unmatch = false;
---   max_abbr_width = 1000;
---   max_kind_width = 1000;
---   max_menu_width = 10000000;
---   documentation = true;
---
---   source = {
---     path = true;
---     buffer = true;
---     calc = true;
---     vsnip = true;
---     nvim_lsp = true;
---     nvim_lua = true;
---     spell = true;
---     tags = true;
---     snippets_nvim = true;
---     treesitter = true;
---   };
--- }
-
-
 -- Setup nvim-cmp.
 local cmp = require'cmp'
 
