0a076b203b9b84529da23534e148d97cd3b6b05c
Oscar Rodriguez <orodriguez@conquestcyber.com>
2022-01-18 11:06:02 -0500

    rollback

--- nvim/lua/myconfig.lua
+++ nvim/lua/myconfig.lua
@@ -84,26 +84,7 @@ cmp.setup({
       i = cmp.mapping.abort(),
       c = cmp.mapping.close(),
     }),
-    -- ['<Tab>'] = function(fallback)
-    --   if cmp.visible() then
-    --     cmp.select_next_item()
-    --   elseif luasnip.expand_or_jumpable() then
-    --     vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-expand-or-jump', true, true, true), '')
-    --     -- luasnip.expand_or_jump()
-    --   else
-    --     fallback()
-    --   end
-    -- end,
-    -- ['<S-Tab>'] = function(fallback)
-    --   if cmp.visible() then
-    --     cmp.select_prev_item()
-    --   elseif luasnip.jumpable(-1) then
-    --     vim.fn.feedkeys(vim.api.nvim_replace_termcodes('<Plug>luasnip-jump-prev', true, true, true), '')
-    --     -- luasnip.jump(-1)
-    --   else
-    --     fallback()
-    --   end
-    -- end,
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
   },
   sources = {
     { name = 'nvim_lsp' },
