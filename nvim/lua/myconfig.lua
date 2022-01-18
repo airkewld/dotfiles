e5360726282228f9405750d1e1210885747cadca
Oscar Rodriguez <orodriguez@conquestcyber.com>
2022-01-18 11:15:07 -0500

    try more

--- nvim/lua/myconfig.lua
+++ nvim/lua/myconfig.lua
@@ -109,12 +109,20 @@ cmp.setup({
       end
     end,
   },
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-    { name = 'nvim_lua' },
-    { name = 'path' },
-    { name = 'buffer', keyword_length = 5 },
+  formatting = {
+    format = lspkind.cmp_format {
+      with_text = true,
+      menu = {
+        buffer   = "[Buffer]",
+        nvim_lsp = "[LSP]",
+        nvim_lua = "[API]",
+        path     = "[Path]",
+        luasnip  = "[Snippet]"
+      },
+    },
+  },
+  experimental = {
+    native_menu = false,
+    ghost_text = true,
   },
 })
-
