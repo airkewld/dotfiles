601f0af667a2e0e2e94c055ffde83333a8077428
Oscar Rodriguez <orodriguez@conquestcyber.com>
2022-03-18 18:03:23 -0400

    add personal remaps and set up treesitter

--- nvim/lua/config/fugitive.lua
+++ nvim/lua/config/fugitive.lua
@@ -1,7 +1,7 @@
 -- vim.api.nvim_set_keymap('n', '<space>G', ':G', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>G', [[<cmd>:G<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>gp', [[<cmd>:G push<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>gc', [[<cmd>:G checkout .<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>gco', [[<cmd>:G checkout .<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>gpp', [[<cmd>:G pull<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>gf', [[<cmd>:G fetch origin '*:*'<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>gfe', [[<cmd>:G fetch origin '*:*'<CR>]], { noremap = true, silent = true })
 
