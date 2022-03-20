ba5b5cad0607484a69c8e75e8ae8745ab80d4f99
Oscar Rodriguez <oscar.rod091@gmail.com>
2022-03-20 10:12:32 -0400

    fix role names

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,7 +2,7 @@
 
 ## Install packages needed to build local config
 # Brew
-if ! which brew
+if ! which brew > /dev/null
 then
   echo "Installing homebrew..."
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -11,17 +11,17 @@ else
 fi
 
 # Neovim
-if ! which nvim
-then
-  echo "Installing neovim..."
-  sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-else
-  echo "neovim already installed."
-fi
+# if ! which nvim > /dev/null
+# then
+#   echo "Installing neovim..."
+#   brew install neovim
+#   brew link neovim
+# else
+#   echo "neovim already installed."
+# fi
 
 # ansible
-if ! which ansible
+if ! which ansible > /dev/null
 then
   echo "installing ansible..."
   brew install ansible
@@ -29,16 +29,8 @@ else
   echo "ansible already installed."
 fi
 
-# oh-my-zsh
-if ! stat /Users/__USERNAME__/.oh-my-zsh
-then
-  echo "Installing oh-my-zsh..."
-  /bin/bash  -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-else
-  echo "oh-my-zsh already installed."
-fi
 
-npm install -g dockerfile-language-server-nodejs vim-language-server
+# npm install -g dockerfile-language-server-nodejs vim-language-server
 
 ## Run playbook
 ansible-playbook config.yaml --diff
