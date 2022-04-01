2dbb635f43493545d020fcbb8d9dd197b3dde4e8
Oscar Rodriguez <orodriguez@conquestcyber.com>
2022-04-01 09:39:46 -0400

    other remaps sucked

--- nvim/lua/config/fugitive.lua
+++ nvim/lua/config/fugitive.lua
@@ -1,7 +1,7 @@
 -- vim.api.nvim_set_keymap('n', '<space>G', ':G', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>G', [[<cmd>:G<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>push', [[<cmd>:G push<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>gps', [[<cmd>:G push<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>gco', [[<cmd>:G checkout .<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>pull', [[<cmd>:G pull<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>gpl', [[<cmd>:G pull<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>gfe', [[<cmd>:G fetch origin '*:*'<CR>]], { noremap = true, silent = true })
 
