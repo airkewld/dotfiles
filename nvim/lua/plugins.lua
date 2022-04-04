b733d1c299199539cdb72dc47f4b0ed51efae579
Oscar Rodriguez <orodriguez@conquestcyber.com>
2022-04-04 10:33:23 -0400

    file browser is the best

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -90,15 +90,6 @@ return require('packer').startup({function()
     -- Logstash syntax support
     use 'robbles/logstash.vim'
 
-    -- neo-tree
-    use {"nvim-neo-tree/neo-tree.nvim",
-    branch = "v2.x",
-    requires = {
-      "nvim-lua/plenary.nvim",
-      "kyazdani42/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim"
-      }
-    }
 end,
 config = {
   display = {
