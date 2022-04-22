32798ccf75309c6c03b5a0f98cd920c341edd41c
Oscar Rodriguez <oscar@macbook-pro.lan>
2022-04-22 10:18:10 -0400

    use fugitive for pull actions

--- nvim/lua/config/git.lua
+++ nvim/lua/config/git.lua
@@ -2,7 +2,7 @@
 vim.api.nvim_set_keymap('n', '<leader>gg', [[<cmd>:LazyGit<CR>]], { noremap = true, silent = true })
 
 -- vim.api.nvim_set_keymap('n', '<leader>gps', [[<cmd>:G push<CR>]], { noremap = true, silent = true })
--- vim.api.nvim_set_keymap('n', '<leader>gpl', [[<cmd>:G pull<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>gp', [[<cmd>:G pull<CR>]], { noremap = true, silent = true })
 
 vim.api.nvim_set_keymap('n', '<leader>gco', [[<cmd>:G checkout .<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>gfe', [[<cmd>:G fetch origin '*:*'<CR>]], { noremap = true, silent = true })
