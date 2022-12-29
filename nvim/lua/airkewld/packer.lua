25e1eb735f22284aa9f9f60618dc25a76d0e3ebd
Oscar Rodriguez <airkewld@oscars-air.lan>
2022-12-29 16:28:16 -0500

    use line wrap and smooth scrolling

--- nvim/lua/airkewld/packer.lua
+++ nvim/lua/airkewld/packer.lua
@@ -99,6 +99,13 @@ return require('packer').startup({function(use)
 
     -- vimwiki
     use 'vimwiki/vimwiki'
+
+    -- smooth Scrolling
+    use {'karb94/neoscroll.nvim',
+      config = function()
+          require('neoscroll').setup()
+      end
+    }
 end,
 config = {
   display = {
