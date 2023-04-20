a1b54600b50963013a6511b2f52f33606ad82015
Oscar Rodriguez <oscar@rebelliondefense.com>
2023-04-20 09:01:36 -0400

    use diff mapping for nvim tree

--- nvim/after/plugin/nvim-tree.lua
+++ nvim/after/plugin/nvim-tree.lua
@@ -1,1 +1,1 @@
-vim.api.nvim_set_keymap('n', '<leader>fb', [[<cmd>:NvimTreeToggle<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-n>', [[<cmd>:NvimTreeToggle<CR>]], { noremap = true, silent = true })
