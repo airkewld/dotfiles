cdb46b6784ab5dfa44dde0571a606cdfee999647
Oscar Rodriguez <orodriguez@chegg.com>
2023-06-09 16:20:23 -0400

    exit if no dir is selected

--- tmux/tmux.sh
+++ tmux/tmux.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-if [[ $# -eq 1 ]]; then
-    selected=$1
+if [[ $(pwd) != ~ ]]; then
+  selected=$(find ~/work ~/dev ~/dotfiles -maxdepth 4 -mindepth 1 -type d | fzf)
 else
-    if [[ $(pwd) != ~ ]]; then
-      selected=$(find ~/work ~/dev ~/dotfiles -maxdepth 4 -mindepth 1 -type d | fzf)
-    else
-      selected=$(find ~ -maxdepth 2 -mindepth 1 -type d | fzf)
-    fi
+  selected=$(find ~ -maxdepth 2 -mindepth 1 -type d | fzf)
+fi
+
+# exit if no directory is selected
+if [[ -z "$selected" ]]; then
+  exit 0
 fi
 
 tmux has-session -t "🧔🏻‍♂️" 2> /dev/null || TMUX='' tmux new-session -d -c "$selected" -s "🧔🏻‍♂️"
 
 if [[ -z "$TMUX" ]];
 then
-tmux attach-session -d -t 🧔🏻‍♂️
+  tmux attach-session -d -t 🧔🏻‍♂️
 else
-tmux new-window -c "$selected"
+  tmux new-window -c "$selected"
 fi
