26486f62a74fc60ac3811991060aa653140979bb
Oscar Rodriguez <orodriguez@chegg.com>
2023-06-09 15:14:27 -0400

    avoid permission issues when going in too deep into the fs

--- tmux/tmux.sh
+++ tmux/tmux.sh
@@ -3,7 +3,11 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~ -maxdepth 2 -mindepth 1 -type d | fzf)
+    if [[ $(pwd) = ~/work ]]; then
+      selected=$(find ~/work ~/dev ~/dotfiles -maxdepth 4 -mindepth 1 -type d | fzf)
+    else
+      selected=$(find ~ -maxdepth 1 -mindepth 1 -type d | fzf)
+    fi
 fi
 
 tmux has-session -t "🧔🏻‍♂️" 2> /dev/null || TMUX='' tmux new-session -d -c "$selected" -s "🧔🏻‍♂️"
