6d62c68ce35536059266ac673c4ec357b8e2cf3c
Oscar Rodriguez <orodriguez@chegg.com>
2023-07-28 12:27:21 -0400

    a bit of customization for fun

--- nvim/after/plugin/floaterm.lua
+++ nvim/after/plugin/floaterm.lua
@@ -1,6 +1,6 @@
 -- cwd term
-vim.api.nvim_set_keymap('n', '<leader>ft', [[<cmd>:FloatermToggle<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ft', [[<cmd>:FloatermNew --name=cwd --title=breaking-things <CR>]], { noremap = true, silent = true })
 -- sleep
-vim.api.nvim_set_keymap('n', '<leader>cff', [[<cmd>:FloatermNew --name=sleep caffeinate -d<CR>]],{ noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>cff', [[<cmd>:FloatermNew --name=sleep --title=this_is_fine... cat ~/dotfiles/art/thisIsFine.txt && caffeinate -d<CR>]],{ noremap = true, silent = true })
 -- tmux
-vim.api.nvim_set_keymap('n', '<leader>tm', [[<cmd>:FloatermNew --name=tmux ~/.config/tmux.sh<CR>]],{ noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>tm', [[<cmd>:FloatermNew --name=tmux --title=tmux-windos ~/.config/tmux.sh<CR>]],{ noremap = true, silent = true })
