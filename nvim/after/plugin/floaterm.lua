f3a236d984cbb0a37c3e90877a93e704d1801935
Oscar Rodriguez <orodriguez@chegg.com>
2023-07-28 12:05:41 -0400

    actually not a good implementation

--- nvim/after/plugin/floaterm.lua
+++ nvim/after/plugin/floaterm.lua
@@ -1,5 +1,5 @@
 -- cwd term
--- vim.api.nvim_set_keymap('n', '<leader>ft', [[<cmd>:FloatermToggle<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ft', [[<cmd>:FloatermToggle<CR>]], { noremap = true, silent = true })
 -- sleep
 vim.api.nvim_set_keymap('n', '<leader>cff', [[<cmd>:FloatermNew --name=sleep caffeinate -d<CR>]],{ noremap = true, silent = true })
 -- tmux
