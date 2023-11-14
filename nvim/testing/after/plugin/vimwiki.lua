8c3e60776665b3958470ae61437fdeff35a4dd34
Oscar Rodriguez <orodriguez@chegg.com>
2023-11-14 11:51:23 -0500

    feat: moved to lazyvim

---
+++ nvim/testing/after/plugin/vimwiki.lua
@@ -0,0 +1,2 @@
+-- vimwiki
+vim.api.nvim_set_keymap('n', '<leader>lt', [[<cmd>:VimwikiToggleListItem<CR>]], { noremap = true, silent = true })
---
+++ nvim/testing/after/plugin/window.lua
@@ -0,0 +1,1 @@
+vim.api.nvim_set_keymap('n', '<leader>wm', [[<cmd>:WindowsMaximize<CR>]], { noremap = true, silent = true })
