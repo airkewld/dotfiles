2abdc0102c5aff3c915ce388072217946e408fc9
Oscar Rodriguez <orodriguez@chegg.com>
2023-11-14 11:51:23 -0500

    feat: moved to lazyvim

---
+++ nvim/testing/after/plugin/mappings.lua
@@ -0,0 +1,25 @@
+--- behave like C and D
+vim.keymap.set('n', 'Y', 'y$')
+
+--- keep it centered
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+vim.keymap.set('n', 'J', 'mzJ`z')
+
+--- undo breakpoints
+vim.keymap.set('i', ',', ',<c-g>u')
+vim.keymap.set('i', '.', '.<c-g>u')
+vim.keymap.set('i', '[', '[<c-g>u')
+vim.keymap.set('i', '!', '!<c-g>u')
+vim.keymap.set('i', '?', '?<c-g>u')
+
+--- window navigation with vim keys
+vim.keymap.set('n', '<leader>h', '<cmd>wincmd h<CR>')
+vim.keymap.set('n', '<leader>j', '<cmd>wincmd j<CR>')
+vim.keymap.set('n', '<leader>k', '<cmd>wincmd k<CR>')
+vim.keymap.set('n', '<leader>l', '<cmd>wincmd l<CR>')
+vim.keymap.set('n', '<leader>pv', '<cmd>wincmd v<bar> :Ex <bar> :vertical resize 30<CR>')
+
+-- quick saves
+vim.api.nvim_set_keymap('n', '<C-s>', [[<cmd>:w<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<C-g>', [[<cmd>:FloatermNew --name=tmux /opt/homebrew/bin/go-search<CR>]],{ noremap = true, silent = true })
---
+++ nvim/testing/after/plugin/telescope.lua
@@ -0,0 +1,101 @@
+-- You dont need to set any of these options. These are the default ones. Only
+-- the loading is important
+--
+--
+local fb_actions = require "telescope._extensions.file_browser.actions"
+
+require('telescope').setup {
+    defaults = {
+        prompt_prefix = "🔍 ",
+        vimgrep_arguments = {
+            'rg',
+            '--color=never',
+            '--no-heading',
+            '--with-filename',
+            '--line-number',
+            '--column',
+            '--smart-case',
+            '--hidden',
+        },
+        mappings = {
+            i = {
+                ['<C-u>'] = false,
+                ['<C-d>'] = false,
+            },
+        },
+    },
+    extensions = {
+        file_browser = {
+            theme = "ivy",
+            hijack_netrw = false,
+            depth = 3,
+            collapse_dirs = true,
+            hidden = { file_browser = true, folder_browser = true },
+            mappings = {
+                ["i"] = {
+                    -- your custom insert mode mappings
+                    ["<C-n>"] = fb_actions.create,
+                },
+                ["n"] = {
+                    -- your custom normal mode mappings
+                },
+            },
+        },
+    }
+}
+-- To get fzf loaded and working with telescope, you need to call
+-- load_extension, somewhere after setup function:
+
+-- Telescope integration for git worktrees
+require("telescope").load_extension("git_worktree")
+
+-- Telescope file browser improved
+require("telescope").load_extension("file_browser")
+
+--Add leader shortcuts
+vim.keymap.set('n', '<leader><space>', [[<cmd>lua require('telescope.builtin').buffers()<CR>]],
+    { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>ff',
+    [[<cmd>lua require('telescope.builtin').find_files({find_command = {"rg", "--ignore", "--hidden", "--files"}})<CR>]],
+    -- [[<cmd>lua require('telescope.builtin').find_files({find_command="rg","--ignore","--hidden","--files",prompt_prefix="🔍"})<CR>]],
+    { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>df',
+    [[<cmd>lua require('telescope.builtin').find_files({prompt_title = "<Dotfiles>", cwd = "~/dotfiles"})<CR>]],
+    { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>bf', [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>]],
+    { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>lg', [[<cmd>lua require('telescope.builtin').live_grep()<CR>]],
+    { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>gwl', [[<cmd>lua require('telescope').extensions.git_worktree.git_worktrees()<CR>]],
+    { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>gwn', [[<cmd>lua require('telescope').extensions.git_worktree.create_git_worktree()<CR>]],
+    { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>gf', [[<cmd>lua require('telescope.builtin').git_files()<CR>]],
+    { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>gc', [[<cmd>lua require('telescope.builtin').git_commits()<CR>]],
+    { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>gb', [[<cmd>lua require('telescope.builtin').git_branches()<CR>]],
+    { noremap = true, silent = true })
+-- telescope file browser
+vim.keymap.set('n', '<leader>fb', [[<cmd>lua require('telescope').extensions.file_browser.file_browser()<CR>]],
+    { noremap = true, silent = true })
+-- quickfix commands
+vim.keymap.set('n', '<leader>cn', [[<cmd>:cn <CR>]], { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>cp', [[<cmd>:cp <CR>]], { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>cc', [[<cmd>:cclose <CR>]], { noremap = true, silent = true })
+
+-- file_browser docs
+
+-- <A-c>/c	Create file/folder at current path (trailing path separator creates folder)
+-- <A-r>/r	Rename multi-selected files/folders
+-- <A-m>/m	Move multi-selected files/folders to current path
+-- <A-y>/y	Copy (multi-)selected files/folders to current path
+-- <A-d>/d	Delete (multi-)selected files/folders
+-- <C-o>/o	Open file/folder with default system application
+-- <C-g>/g	Go to parent directory
+-- <C-e>/e	Go to home directory
+-- <C-w>/w	Go to current working directory (cwd)
+-- <C-t>/t	Change nvim's cwd to selected folder/file(parent)
+-- <C-f>/f	Toggle between file and folder browser
+-- <C-h>/h	Toggle hidden files/folders
+-- <C-s>/s	Toggle all entries ignoring ./ and ../
