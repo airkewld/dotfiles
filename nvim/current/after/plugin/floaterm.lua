21e244774266b966bcc149fe9c8e612761dc36a5
Oscar Rodriguez <orodriguez@chegg.com>
2023-11-17 16:58:14 -0500

    fix: typo in window name

--- nvim/current/after/plugin/floaterm.lua
+++ nvim/current/after/plugin/floaterm.lua
@@ -3,4 +3,4 @@ vim.api.nvim_set_keymap('n', '<leader>ft', [[<cmd>:FloatermNew --name=cwd --titl
 -- sleep
 vim.api.nvim_set_keymap('n', '<leader>cff', [[<cmd>:FloatermNew --name=sleep --title=this_is_fine... lolcat ~/dotfiles/art/thisIsFine.txt && caffeinate -d<CR>]],{ noremap = true, silent = true })
 -- tmux
-vim.api.nvim_set_keymap('n', '<leader>tm', [[<cmd>:FloatermNew --name=tmux --title=tmux-windos ~/.config/tmux.sh<CR>]],{ noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>tm', [[<cmd>:FloatermNew --name=tmux --title=tmux-windows ~/.config/tmux.sh<CR>]],{ noremap = true, silent = true })
