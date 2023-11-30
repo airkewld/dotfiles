066683b04e60798f71f0531868ef6978c10faedc
Oscar Rodriguez <orodriguez@chegg.com>
2023-11-30 16:29:56 -0500

    fix: make it easier to read and update

--- nvim/current/lua/treesitter-setup.lua
+++ nvim/current/lua/treesitter-setup.lua
@@ -4,7 +4,21 @@
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash', 'terraform', 'yaml', 'json', 'html', 'css', 'dockerfile', 'java', 'toml', 'lua', 'hcl' },
+    ensure_installed = {
+      'go',
+      'lua',
+      'python',
+      'vimdoc',
+      'vim',
+      'bash',
+      'terraform',
+      'yaml',
+      'json',
+      'dockerfile',
+      'toml',
+      'lua',
+      'hcl',
+    },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
