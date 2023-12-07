773b250f1b720de482504c6976bc463c5def5530
Oscar Rodriguez <orodriguez@chegg.com>
2023-12-07 12:30:11 -0500

    fix: update terminal window titles

--- nvim/old-packer/after/plugin/floaterm.lua
+++ nvim/old-packer/after/plugin/floaterm.lua
@@ -1,6 +1,6 @@
 -- cwd term
-vim.api.nvim_set_keymap('n', '<leader>ft', [[<cmd>:FloatermNew --name=cwd --title=breaking-things <CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ft', [[<cmd>:FloatermNew --name=cwd --title=breaking_things <CR>]], { noremap = true, silent = true })
 -- sleep
-vim.api.nvim_set_keymap('n', '<leader>cff', [[<cmd>:FloatermNew --name=sleep --title=this_is_fine... lolcat ~/dotfiles/art/thisIsFine.txt && caffeinate -d<CR>]],{ noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>cff', [[<cmd>:FloatermNew --name=sleep --title=taking_a_break... lolcat ~/dotfiles/art/thisIsFine.txt && caffeinate -d<CR>]],{ noremap = true, silent = true })
 -- tmux
-vim.api.nvim_set_keymap('n', '<leader>tm', [[<cmd>:FloatermNew --name=tmux --title=tmux-windos ~/.config/tmux.sh<CR>]],{ noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>tm', [[<cmd>:FloatermNew --name=tmux --title=open_new_project ~/.config/tmux.sh<CR>]],{ noremap = true, silent = true })
