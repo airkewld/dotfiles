1b1667a7b1df4445b96e933ddfb67939b6ea9017
Oscar Rodriguez <orodriguez@chegg.com>
2023-12-15 10:11:37 -0500

    fix: better session management

--- nvim/current/after/plugin/floaterm.lua
+++ nvim/current/after/plugin/floaterm.lua
@@ -4,4 +4,4 @@ vim.api.nvim_set_keymap('n', '<leader>ft', [[<cmd>:FloatermNew --name=cwd --titl
 vim.api.nvim_set_keymap('n', '<leader>cff', [[<cmd>:FloatermNew --name=sleep --title=taking_a_break... lolcat ~/dotfiles/art/thisIsFine.txt && caffeinate -d<CR>]],{ noremap = true, silent = true })
 -- tmux
 vim.api.nvim_set_keymap('n', '<leader>tm', [[<cmd>:FloatermNew --name=tmux --title=open_new_project ~/.config/new-windows.sh<CR>]],{ noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>ts', [[<cmd>:FloatermNew --name=tmux --title=open_new_project ~/.config/new-session.sh<CR>]],{ noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>ts', [[<cmd>:FloatermNew --name=tmux --title=open_new_session ~/.config/new-session.sh<CR>]],{ noremap = true, silent = true })
