71848e6888a165b2331251406667378e64bc3050
Oscar Rodriguez <orodriguez@chegg.com>
2024-01-30 15:38:38 -0500

    fix: update keymap for harpoon

--- nvim/current/after/plugin/harpoon.lua
+++ nvim/current/after/plugin/harpoon.lua
@@ -1,4 +1,4 @@
-vim.api.nvim_set_keymap('n', '<leader>ha', [[<cmd>lua require('harpoon.mark').add_file()<CR>]],
+vim.api.nvim_set_keymap('n', '<leader>hh', [[<cmd>lua require('harpoon.mark').add_file()<CR>]],
     { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>hm', [[<cmd>lua require('harpoon.ui').toggle_quick_menu()<CR>]],
     { noremap = true, silent = true })
