27248619e4791a0b73ecfa6e5c9f8f0b48b96894
Oscar Rodriguez <orodriguez@chegg.com>
2024-08-07 12:17:41 -0400

    fix: add gen ai plugin

---
+++ nvim/current/after/plugin/gp.lua
@@ -0,0 +1,3 @@
+vim.api.nvim_set_keymap('n', '<leader>ch', [[<cmd>:GpChatNew vsplit<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>cr', [[<cmd>:GpChatRespond<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>gcc', [[<cmd>:GpNew<CR>]], { noremap = true, silent = true })
